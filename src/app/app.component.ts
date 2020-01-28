213a80605a6cf6ee8cc2ee3cadc0184ab961ee6e
GuillaumeCz <115c7a5fac@pm.me>
2020-01-28 15:24:11 +0100

    feat: 🎸 Web3 integration example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Web3Service, IBalance} from './web3.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'openToken';
+  addresses$: Observable<Array<string>>; 
+  balances$: Observable<Array<IBalance>>;
+
+  constructor(private web3Service: Web3Service) {}
+
+  ngOnInit() {
+    this.addresses$ = this.web3Service.getAddresses();
+    this.balances$ = this.web3Service.getBalances();
+  }
+
 }
---
+++ src/app/web3.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import Web3 from 'web3';
+import { Observable, from } from 'rxjs';
+
+export interface IBalance {
+  address: string;
+  balance: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Web3Service {
+
+  private instance: Web3;
+
+  constructor() { 
+    this.instance = new Web3('ws://localhost:8545');
+  }
+
+  getInstance() {
+    return this.instance;
+  }
+
+  getAddresses(): Observable<Array<string>> {
+    return from(this.instance.eth.getAccounts());
+  }
+
+  getBalances(): Observable<Array<IBalance>> {
+    return from(this.instance.eth.getAccounts().then((accounts) => {
+      let balances: Array<IBalance> = [];
+      for(let account of accounts) {
+        this.instance.eth.getBalance(account).then(balance => {
+          const b: IBalance = {
+            address: account,
+            balance
+          };
+          balances.push(b)
+        })
+      }
+      return balances;
+    }))
+  }
+}
