56057af0a5d38b522ca0b442eff24611499ec613
GuillaumeCz <115c7a5fac@pm.me>
2020-04-10 14:43:45 +0200

    Add Abstract Contract service

---
+++ src/app/services/contract.service.ts
@@ -0,0 +1,27 @@
+import { Contract } from "web3-eth-contract";
+import {Web3Service} from "./web3Service/web3.service";
+import {Injectable} from "@angular/core";
+
+@Injectable({
+  providedIn: "root"
+})
+export abstract class ContractService {
+  instance: Contract;
+  account: string;
+
+  constructor(protected web3Service: Web3Service) {
+    this.web3Service.accountsObservable
+      .subscribe(account => {
+        this.account = account[0];
+      });
+  }
+
+  public abstract async init();
+
+  protected initWeb3(artifact: any) {
+    return this.web3Service.artifactToContract(artifact)
+      .then(instance => {
+        this.instance = instance;
+      });
+  }
+}
---
+++ src/app/services/helloworldService/helloworld.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import {ContractService} from "../contract.service";
+
+declare let require: any;
+const helloWorldArtifacts = require("../../../assets/abi/HelloWorld.json");
+
+@Injectable({
+  providedIn: "root"
+})
+export class HelloworldService extends ContractService {
+
+  public async init() {
+    await this.initWeb3(helloWorldArtifacts);
+  }
+
+  public getDumbValue(): Promise<string> {
+    return this.instance.methods.getDumbValue().call();
+  }
+
+  public setDumbValue(value: string): Promise<any> {
+    return this.instance.methods.setDumbValue(value).send({ from: this.account, gas: 4500000});
+  }
+}
