dcbbc1ba1911f301afd6103f47ec556a99354c23
GuillaumeCz <115c7a5fac@pm.me>
2020-04-10 14:56:31 +0200

    Refactor helloworld component

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Web3Service } from "../services/web3Service/web3.service";
-import { Contract } from "web3-eth-contract";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
-import {MatSnackBar} from '@angular/material';
-
-declare let require: any;
-const helloWorldArtifacts = require("../../assets/abi/HelloWorld.json");
+import {MatSnackBar} from "@angular/material";
+import {HelloworldService} from "../services/helloworldService/helloworld.service";
 
 @Component({
   selector: "app-hello-world",
@@ -13,56 +10,35 @@ const helloWorldArtifacts = require("../../assets/abi/HelloWorld.json");
   styleUrls: ["./hello-world.component.scss"]
 })
 export class HelloWorldComponent implements OnInit {
-  HelloWorldInstance: Contract;
   dumbValue: string;
-  account: string;
   form: FormGroup;
 
-  constructor(private web3Service: Web3Service, private snackBar: MatSnackBar) {}
+  isReady = false;
+
+  constructor(private helloworldService: HelloworldService, private snackBar: MatSnackBar) {}
 
   ngOnInit() {
-    this.watchAccount();
-    this.web3Service
-      .artifactToContract(helloWorldArtifacts)
-      .then(instance => {
-        this.HelloWorldInstance = instance;
-        return this.HelloWorldInstance.methods.getDumbValue().call();
-      })
-      .then(dumb => {
-        this.dumbValue = dumb;
-      });
-
+    this.helloworldService.init().then(() => {
+      this.isReady = true;
+      return this.helloworldService.getDumbValue();
+    }).then((dumb) => this.dumbValue = dumb);
     this.form = new FormGroup({
       newValue: new FormControl("", Validators.required)
     });
   }
 
-  watchAccount() {
-    this.web3Service.accountsObservable.subscribe(accounts => {
-      this.account = accounts[0];
-    });
-  }
-
   sendTx() {
-    this.HelloWorldInstance.methods
-      .setDumbValue(this.form.getRawValue().newValue)
-      .send({ from: this.account, gas: 4500000 })
+    const v = this.form.getRawValue().newValue;
+    this.helloworldService.setDumbValue(v)
       .then(() => {
-        this.snackBar.open("Success ! :D", "X")
+        this.snackBar.open("Success ! :D", "X");
         this.form.reset();
         this.refreshDumbValue();
-      })
-      .catch(err => {
-        this.snackBar.open("Something went wrong ! :(", "X");
       });
+
   }
 
   refreshDumbValue(): void {
-    this.HelloWorldInstance.methods
-      .getDumbValue()
-      .call()
-      .then(dumb => {
-        this.dumbValue = dumb;
-      });
+    this.helloworldService.getDumbValue().then(dumb => this.dumbValue = dumb);
   }
 }
