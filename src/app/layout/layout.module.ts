c76586938c73e710fcf31cc6981a9de546f4b198
GuillaumeCz <115c7a5fac@pm.me>
2020-04-17 10:52:32 +0200

    Fix to the router

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from "@angular/common";
 import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./header/header.component";
 import { MaterialModule } from "../material/material.module";
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [FooterComponent, HeaderComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, RouterModule ],
   exports: [FooterComponent, HeaderComponent]
 })
 export class LayoutModule {}
